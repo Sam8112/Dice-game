c3429f37caf780dcba09d69815dbb5fefc1cbfea
Sameer Sharma <118039352+Sam8112@users.noreply.github.com>
2024-05-07 16:56:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// player dice 1
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1 = "dice"+ randomNumber1 + ".png";
+
+var randomImageSource1 = "images/"+ randomDiceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource1);
+
+// player 2 dice
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice"+ randomNumber2 + ".png";
+
+var randomImageSource2 = "images/"+ randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+} else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}else{
+    document.querySelector("h1").innerHTML="It's a Draw!";
+}
